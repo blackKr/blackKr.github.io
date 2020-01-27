0345233c039eef9afc66f32db6a217bfe2de661b
wjc201314 <1756934743@qq.com>
2020-01-28 01:25:37 +0800

    Site updated: 2020-01-28 01:25:35

---
+++ aPlayer/music.js
@@ -0,0 +1,17 @@
+
+
+const ap = new APlayer({
+  container: document.getElementById('aplayer'),
+  fixed: false,
+  mini: false,
+  autoplay: true,
+  lrcType: 3,
+  audio: [
+    {
+      "name": "沙漠骆驼",
+      "artist": "展展与罗罗",
+      "url": "http://m10.music.126.net/20200128012129/3013e0625a51de6fbed5e2da37760d9e/ymusic/f351/ae94/e2e6/9688d9e8a5fdb917efbadbb57b5e6ecd.mp3",
+      "cover": "https://p3.music.126.net/oov7j64hTKZSm0CEUkRNoQ==/109951163111639178.jpg?param=300y300"
+    }
+  ]
+});
